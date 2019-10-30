c45dc2d7260c88967b059b4ead953d64a0c7b418
bbsmax <bbsmax@nate.com>
2019-10-31 06:03:02 +0900

    user model 추가

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 # Register your models here.
+
+#class CustomUserAdmin(admin.ModelAdmin): //사용자가 정의한 UserAdmin을 사용함.
 @admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields" : (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "language",
+                    "currency",
+                    "birthdate",
+                 )
+            }
+        ),
+    )
--- user/models.py
+++ user/models.py
@@ -3,7 +3,34 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_KOREAN = "korean"
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "kr"),
+        (LANGUAGE_ENGLISH, "en"),
+    )
+
+    CURRENCY_KWD = "kwd"
+    CURRENCY_USD = "usd"
+    CURRENCY_CHOICES = (
+        (CURRENCY_KWD, "kwd"),
+        (CURRENCY_USD, "usd"),
+    )
+
     avatar = models.ImageField(blank=True, null=True)
     bio = models.TextField(default="", blank=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True, null=True)
+    language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, blank=True, null=True)
+    birthdate = models.DateField(blank=True, null=True)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, blank=True, null=True)
     superhost = models.BooleanField(default=False, blank=True)
     pass
